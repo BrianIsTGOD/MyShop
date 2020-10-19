f1f8d803b9622e28d261fa6b6ff90deba512838e
BrianIsTGOD <heartgoldkid@gmail.com>
2020-10-18 22:50:40 -0500

    Added Basket Entity

---
+++ MyShop/MyShop.Core/Models/BasketItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class BasketItem: BaseEntity
+    {
+        public string BasketId { get; set; }
+        public string ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+
+}
--- MyShop/MyShop.DataAccess.SQL/DataContext.cs
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -15,8 +15,10 @@ namespace MyShop.DataAccess.SQL
         {
 
         }
-       
+
         public DbSet<Product> Products { get; set; }
-        public DbSet <ProductCategory> ProductCategories { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Basket>  Baskets{get; set;}
+        public DbSet<BasketItem> BasketItems { get; set; }
     }
 }
