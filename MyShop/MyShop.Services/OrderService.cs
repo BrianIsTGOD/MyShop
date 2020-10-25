e7c4683a025274119685c2d08e401e575637459a
BrianIsTGOD <heartgoldkid@gmail.com>
2020-10-24 20:39:13 -0500

    Created Order Admin View

--- MyShop/MyShop.Services/OrderService.cs
+++ MyShop/MyShop.Services/OrderService.cs
@@ -35,5 +35,21 @@ namespace MyShop.Services
             orderContext.Insert(baseOrder);
             orderContext.Commit();
         }
+
+        public List<Order> GetOrderList()
+        {
+            return orderContext.Collection().ToList();
+        }
+
+        public Order GetOrder(string Id)
+        {
+            return orderContext.Find(Id);
+        }
+
+        public void UpdateOrder(Order updateOrder)
+        {
+            orderContext.Update(updateOrder);
+            orderContext.Commit();
+        }
     }
 }
